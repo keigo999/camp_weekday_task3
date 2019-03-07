c182943c529e7fe18a74a554df1e2e88551a9615
keigo999 <highbridgek5@gmail.com>
2019-03-07 17:20:00 +0900

    ルートの削除とシンボル名の変更

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,8 +1,8 @@
 class CalcsController < ApplicationController
   include CalcsHelper
   def show
-    @num1 = params[:price1].to_i
-    @num2 = params[:price2].to_i
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
     @operator = params[:oprator]
     calculation
   end
