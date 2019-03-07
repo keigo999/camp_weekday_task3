e67dad5c05d9447f4534d54bb1f71aff2efb61f7
keigo999 <highbridgek5@gmail.com>
2019-03-07 11:04:51 +0900

    totalメソッドをhelperに移動

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,20 +1,9 @@
 class CalcsController < ApplicationController
+  include CalcsHelper
   def show
     @price1 = params[:price1].to_i
     @price2 = params[:price2].to_i
     @oprator = params[:oprator]
     total
   end
-
-  def total
-    case @oprator
-    when "addition"
-      @total = @price1 + @price2
-    when "subtraction"
-      @total = @price1 - @price2
-    when "multiplication"
-      @total = @price1 * @price2
-    when "division"
-      @total = @price1 / @price2
-    end
 end
--- app/helpers/calcs_helper.rb
+++ app/helpers/calcs_helper.rb
@@ -1,2 +1,18 @@
 module CalcsHelper
+def total
+    @total =  case @oprator
+              when "addition"
+                @price1 + @price2
+              when "subtraction"
+                @price1 - @price2
+              when "multiplication"
+                @price1 * @price2
+              when "division"
+                if @price2 == 0 
+                  "0では割れません"
+                else
+                  @price1 / @price2.to_f
+                end  
+              end
+end
 end
